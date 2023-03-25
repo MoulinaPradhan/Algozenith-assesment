aa85131482747123d32dcc4e7a7b5a2ecbd8a23d
MoulinaPradhan <pmoulina76@gmail.com>
2023-03-25 16:29:20 +0530

    side bar

--- src/Componets/Sidebanner/Sidebanner.js
+++ src/Componets/Sidebanner/Sidebanner.js
@@ -5,24 +5,30 @@ const Sidebanner = () => {
   return (
     <div className='card'>
         <div className='card-header'>
-          <p>Premium Victory Batch</p>
-          <h2><s>₹19,999</s></h2>
-          <h3>₹13,999</h3>
-          <div className='logo'></div>
+          <p className="heading" >Premium Victory Batch</p>
+         <div className='sub-heading'>
+          <div className='left-heading'>
+         <s >  <h2 className='strike-text'><s>₹19,999</s></h2></s>
+           <h1 className="new-rate">₹13,999</h1>
+           </div>
+         
+          <div className='logo-img'>
+          </div>
+          </div>
         </div>
     <div className='checkboxes'>
     <input type="checkbox" id="check1"/>
-    <label for ="check1">Lorem ipsum dolor sit amet</label>
+    <label for ="check1" className='side-label'>100+ Hrs Live Content</label>
     <input type="checkbox" id="check2"/>
-    <label for ="check2">Lorem ipsum dolor sit amet</label>
+    <label for ="check2" className='side-label'>50+ Hrs Contest</label>
     <input type="checkbox" id="check3"/>
-    <label for ="check3">Lorem ipsum dolor sit amet</label>
+    <label for ="check3" className='side-label'>500+ Problems</label>
     <input type="checkbox" id="check4"/>
-    <label for ="check4">Lorem ipsum dolor sit amet</label>
+    <label for ="check4" className='side-label'>Includes an interview bootcamp and access to a mentor network</label>
     <input type="checkbox" id="check1"/>
-    <label for ="check1">Lorem ipsum dolor sit amet</label>
+    <label for ="check1" className='side-label'>Enjoy access to our content for one year (paid extensions available)</label>
     <input type="checkbox" id="check2"/>
-    <label for ="check2">Lorem ipsum dolor sit amet</label>
+    <label for ="check2" className='side-label'>Flexible payment options, including no-cost EMI, are available.</label>
     <div className='button-container'>
 <button className='btn'>Join now &rarr;</button>
         </div>
