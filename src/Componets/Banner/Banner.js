dfcad99bfc4437406ee915b18c3eead74c52c3b0
MoulinaPradhan <pmoulina76@gmail.com>
2023-03-26 14:18:09 +0530

    add responsieveness

--- src/Componets/Banner/Banner.js
+++ src/Componets/Banner/Banner.js
@@ -6,21 +6,25 @@ export const Banner = () => {
   return (
     <div className="sub-container">
     <div className="context">
-<h1>Be Better at DSA & CP</h1>
+<h1>Be better at DSA & CP</h1>
 <p>Lorem ipsum dolar sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
 <div className='button-container'>
-    <button>Join now &rarr; </button>
-    <button>View curriculum</button>
+    <button className='btn join-btn'>Join now &rarr; </button>
+    <button className='btn view'>View curriculum</button>
 </div>
 <div className='checkbox-container'>
-    <input type="checkbox" id="check1"/>
+  <div className='left-side'>
+  <input type="checkbox" id="check1"/>
     <label for ="check1">Lorem ipsum dolor sit amet</label>
-    <input type="checkbox" id="check2"/>
+    <input type="checkbox" id="check2" style={{marginLeft:10}}/>
     <label for ="check2">Lorem ipsum dolor sit amet</label>
+  </div>
+   <div className='right-side'>
     <input type="checkbox" id="check3"/>
     <label for ="check3">Lorem ipsum dolor sit amet</label>
-    <input type="checkbox" id="check4"/>
+    <input type="checkbox" id="check4" style={{marginLeft:10}}/>
     <label for ="check4">Lorem ipsum dolor sit amet</label>
+</div>
 </div>
 </div>
     <img className='logo-container'
